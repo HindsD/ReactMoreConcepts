131304a94ce495e689a0794250354c0d28dfbf6e
Daniel Hinds <dhinds423@gmail.com>
2022-09-19 10:14:32 -0500

    to functional components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React, { useState, useEffect} from 'react'
 import './App.css';
+import axios from 'axios';
 import Country from './components/Country';
 import NewCountry2 from './components/NewCountry2'
 
 const App = () => {
   const [ countries, setCountries ] = useState([]);
+  const apiEndpoint = "https://advanced-web-dwh.azurewebsites.net/api/country";
   useEffect(() => {
-    let fetchedCountries = [
-      { id: 1, name: 'United States', gold: 2, silver: 2, bronze: 3 },
-      { id: 2, name: 'China', gold: 3, silver: 1, bronze: 0 },
-      { id: 3, name: 'Germany', gold: 0, silver: 2, bronze: 2 },
-    ]
-    setCountries(fetchedCountries);
+
+    async function fetchData() {
+      const { data: fetchedCountries } = await axios.get(apiEndpoint);
+      setCountries(fetchedCountries);
+    }
+    fetchData();
+
+    // let fetchedCountries = [
+    //   { id: 1, name: 'United States', gold: 2, silver: 2, bronze: 3 },
+    //   { id: 2, name: 'China', gold: 3, silver: 1, bronze: 0 },
+    //   { id: 3, name: 'Germany', gold: 0, silver: 2, bronze: 2 },
+    // ]
+    // setCountries(fetchedCountries);
   }, []);
 
   const getTotalMedalCount = () => {
@@ -71,13 +80,36 @@ const App = () => {
     }
   }
 
-  const handleDelete = (countryId) => {
-    setCountries(countries.filter((c) => c.id !== countryId));
-  };
-  const handleAdd = (name) => {
+  const handleDelete = async (countryId) => {
+    const originalCountries = countries;
+    setCountries(countries.filter(c => c.id !== countryId));
+    try {
+      await axios.delete(`${apiEndpoint}/${countryId}`);
+    } catch(ex) {
+      alert('An error occurred while deleting a word');
+      setCountries(originalCountries);
+      if (ex.response && ex.response.status === 404) {
+        console.log("The record does not exist - it may have already been deleted");
+      } else { 
+        alert('An error occurred while deleting a country');
+        setCountries(originalCountries);
+      }
+    }
+  }
+
+  // const handleDelete = (countryId) => {
+  //   setCountries(countries.filter((c) => c.id !== countryId));
+  // };
+
+  const handleAdd = async (name) => {
     const id = countries.length === 0 ? 1 : Math.max(...countries.map(country => country.id)) + 1;
-    setCountries(countries.concat({ id: id, name: name, gold: 0, silver: 0, bronze: 0 }));
-  };
+    const { data: post } = await axios.post(apiEndpoint, { id: id, name: name, gold: 0, silver: 0, bronze: 0 });
+    setCountries(countries.concat(post));
+  }
+  // const handleAdd = (name) => {
+  //   const id = countries.length === 0 ? 1 : Math.max(...countries.map(country => country.id)) + 1;
+  //   setCountries(countries.concat({ id: id, name: name, gold: 0, silver: 0, bronze: 0 }));
+  // };
 
     return (
       <div className="App">
